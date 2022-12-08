5d5fef81ebc6c3f8396b9bcbfcfff8561c51ab50
Ceriole <richie0415@hotmail.com>
2022-12-07 23:35:37 -0500

    Updated title screen graphics

--- src/asset.h
+++ src/asset.h
@@ -8,11 +8,14 @@
     extern const uint8_t var_name ## _end[]; \
     extern const void __bank__ ## var_name
 
+
 #define ASSET(var_name, filename) \
     void __ ## var_name ## __() __naked { \
         __asm__("_" #var_name "::"); \
         __asm__(".incbin \"build/res/" filename "\""); \
         __asm__("_" #var_name "_end::"); \
     } EXTERN_ASSET(var_name)
+
+#define sizeof_ASSET(var_name) (var_name ## _end - var_name)
 
 #endif
--- src/main.c
+++ src/main.c
@@ -4,13 +4,37 @@
 
 #include "asset.h"
 
-ASSET(penta, "penta.chr");
+#define TITLE_MAP_WIDTH DEVICE_SCREEN_WIDTH
+#define TITLE_MAP_HEIGHT DEVICE_SCREEN_HEIGHT
+#define TITLE_NUM_TILES (sizeof_ASSET(title) / 16)
+
+ASSET(title, "title.chr");
+ASSET(title_map, "title.map");
+ASSET(title_attr, "title.attrmap");
+
+ASSET(title_pal, "title.pal");
 
 void main(void)
 {
+	set_bkg_palette(BKGF_CGB_PAL0, sizeof_ASSET(title_pal) / 4, title_pal);
+
+	set_bkg_data(0u, TITLE_NUM_TILES, title);
+	/* Select VRAM bank 1 */
+	VBK_REG = VBK_ATTRIBUTES;
+	/* Set attributes */
+	set_bkg_tiles(0, 0, TITLE_MAP_WIDTH, TITLE_MAP_HEIGHT, title_attr);
+
+	/* Select VRAM bank 1 */
+	VBK_REG = VBK_TILES;
+	/* Set attributes */
+	set_bkg_tiles(0, 0, TITLE_MAP_WIDTH, TITLE_MAP_HEIGHT, title_map);
+
+	SHOW_BKG;
+	enable_interrupts();
+	DISPLAY_ON;
+
 	while(1)
 	{
-		
 		wait_vbl_done();
 	}
 }
